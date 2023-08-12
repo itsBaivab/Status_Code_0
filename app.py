1777c87f4491853510339d82aa34fcae4652ef91
Baivab Mukhopadhyay <101830709+itsBaivab@users.noreply.github.com>
2023-08-12 11:59:03 +0000

    controlnet ui

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.set_page_config(page_title="AI QR Code Genarator")
 
 st.title("AI QR Code Generator")
-
+#Form UI
 with st.form(key="form1"):
     st.text('Fill info')
     PF = st.file_uploader('Upload a jpg or jpeg')
@@ -34,3 +34,34 @@ Final = str(FirstName)+'#'+str(LastName)+'#'+str(FathersName)+'#'+str(MothersNam
 if submit:
     st.write(FirstName, LastName, Birthday, FathersName, MothersName, Gender)
     st.success("In the next step enter your prompt")
+#image =Image.open('/tmp/gradio/175a626d1211ba6457ba3d3a4b5bd71b6938b029/tmpo9vdn2tg.png')
+
+
+
+
+# Controlnet UI
+with st.form(key= "form2"):
+    st.text("QR Genaration")
+    # QRCodeContent = st.text_input('')
+    p_prompt = st.text_input('Positive Prompt')
+    n_prompt = st.text_input('Negetive Prompt')
+    conditioning_scale = st.slider('Controlnet Conditioning Scale', 0.0, 5.0)
+    strength = st.slider('Strenght', 0.0, 1.0)
+    guidance_scale = st.slider('Guidance Scale', 1.00, 50.00)
+    sampler = str(st.selectbox('Sampler', ['DPM++ Karras SDE', 'DPM++ Karras', 'Heun', 'Euler', 'DDIM','DEIS']))
+    seed = st.slider('Seed value', -1,  9999999999)
+    genarate = st.form_submit_button(on_click=None)
+positive_prompt = str(p_prompt)
+negative_prompt = str(n_prompt)    
+Sampler= str(sampler)
+Conditioning_scale = float(conditioning_scale)
+Strength = float(strength)
+Guidance_scale = float(guidance_scale)
+Seed = int(seed)
+
+URL =""
+if genarate:
+    genimg = QR_Gen.generate_qr_code(URL,positive_prompt, negative_prompt,Sampler,Strength,Conditioning_scale,Guidance_scale,Seed)
+    image = Image.open(genimg)
+    st.image(image, caption='Genarated QR code')
+    st.success("QR code genarated successfully")
